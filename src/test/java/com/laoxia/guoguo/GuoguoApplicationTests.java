bbec79eab05a96e6d817385a98f39179d1789914
zhangchongchong <317337161@qq.com>
2023-09-21 14:10:40 +0800

    springboot项目初始化

---
+++ src/test/java/com/laoxia/guoguo/GuoguoApplicationTests.java
@@ -0,0 +1,13 @@
+/*package com.laoxia.guoguo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuoguoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}*/
